a96323fb40ab10c0d2753a19094f7aee027833e1
huseyn-hesenov <hesenovhuseyn444@gmail.com>
2021-02-04 00:46:52 +0400

    code

---
+++ Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int factorial=1;
+	cout<<"ededi daxil edin";
+	int eded;
+	cin>>eded;
+	for(int i=1;i<=eded;i++){
+		factorial=factorial*i;
+	}
+	cout<<"factorial:"<<factorial;
+	
+	
+	
+} 
